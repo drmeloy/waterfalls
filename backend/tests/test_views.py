46746971a1a44b3796d22dbf2bbf975a310588f7
drmeloy <drmeloy@gmail.com>
2020-07-07 17:08:02 -0700

    wrote all api get tests

--- backend/tests/test_views.py
+++ backend/tests/test_views.py
@@ -5,6 +5,31 @@ from rest_framework import status
 from ..models import Waterfall
 from ..serializers import WaterfallSerializer
 
+class WaterfallApiGetTests(APITestCase):
+  def setUp(self):
+    Waterfall.objects.create(
+      name='Super Falls', height='555 ft.', longitude='123 N', latitude='321 W', description='Super cool falls')
+    Waterfall.objects.create(
+      name='Lame Falls', height='7 ft.', longitude='987 S', latitude='789 E', description='Pretty lame falls')
+
+  def test_get_all_waterfalls(self):
+    response = self.client.get('/api/waterfalls/')
+    waterfalls = Waterfall.objects.all()
+    serializer = WaterfallSerializer(waterfalls, many=True)
+    self.assertEqual(response.data, serializer.data)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+  def test_get_single_waterfall_valid(self):
+    response = self.client.get('/api/waterfalls/1/')
+    waterfall = Waterfall.objects.get(pk=1)
+    serializer = WaterfallSerializer(waterfall)
+    self.assertEqual(response.data, serializer.data)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+  def test_get_single_waterfall_invalid(self):
+    response = self.client.get('/api/waterfalls/999/')
+    self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
 class WaterfallApiPostTests(APITestCase):
   def test_post_waterfall_valid_data(self):
     valid_data = {"name": "Super Falls", "height": "555 ft.", "longitude": "123 N", "latitude": "321 W", "description": "Super cool falls"}
@@ -31,6 +56,3 @@ class WaterfallApiPostTests(APITestCase):
     response = self.client.post('/api/waterfalls/', invalid_data)
     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-  # def test_get_waterfalls(self):
-  #   response = self.client.get('/api/waterfalls/')
-  #   self.assertEqual(json.loads(response.content), [{"id": 1, "name": "Super Falls", "height": "555 ft.", "longitude": "123 N", "latitude": "321 W", "description": "Super cool falls"}])
