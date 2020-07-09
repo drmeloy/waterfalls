c55878d0f523297f539b6d8ed5f77921b1d1ab3e
drmeloy <drmeloy@gmail.com>
2020-07-08 19:41:06 -0700

    swapped lat and long on model, added custom __str__ method, and modified seed data

--- backend/migrations/0002_waterfalls_seed_data.py
+++ backend/migrations/0002_waterfalls_seed_data.py
@@ -5,11 +5,11 @@ from django.db import migrations
 def seed_data(apps, schema_editor):
     Waterfall = apps.get_model('backend', 'Waterfall')
     Waterfall(
-        name='Silver Falls',
-        height='178 ft',
-        longitude='44.89 N',
-        latitude='122.6461 W',
-        description='Cool waterfall that you can hike behind!').save()
+        name='Silver Falls (South Falls)',
+        height='177 ft',
+        latitude='44.87883',
+        longitude='-122.65887',
+        description="South Falls is THE waterfall in Silver Falls State Park. At 177 feet, it is the second tallest waterfall in the park, and it's the tallest single drop. It's easily accessed by a paved trail from the South Falls Lodge. A number of educational and historic markers dot the area, along with stone walls and benches carved from logs.").save()
 
 class Migration(migrations.Migration):
     dependencies = [
--- backend/models.py
+++ backend/models.py
@@ -3,11 +3,14 @@ from django.db import models
 class Waterfall(models.Model):
     name = models.CharField(max_length=100)
     height = models.CharField(max_length=10)
-    longitude = models.CharField(max_length=10)
     latitude = models.CharField(max_length=10)
+    longitude = models.CharField(max_length=10)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def describe_waterfall(self):
         return self.name + ' is ' + self.height + ' tall and is located at ' + self.longitude + ' and ' + self.latitude + '.'
+
+    def __str__(self):
+        return self.name
